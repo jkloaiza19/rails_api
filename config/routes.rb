398b5acc4260a39a5d34846a5a0cac09f9677bc1
Juan Carlos Loaiza <ubuntu@ip-172-31-57-231.ec2.internal>
2022-08-22 01:11:07 +0000

    initial commit

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,3 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
-  root 'pages#home'
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
